2fa2d8c1392018c692d0cfab6930a282a00bd18c
sogol-tech <afsharsogol@icloud.com>
2021-08-31 15:33:40 +0100

    Created search engine.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p> Hello. I am going to be a dictionary!</p>
-        <button type="button" class="btn btn-secondary">
-          Secondary
-        </button>
-      </header>
+      <div className="container">
+        <Dictionary />
+      </div>
     </div>
   );
 }
