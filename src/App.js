8458ddeb8d91159f5603bd04b4853416ad09017e
sogol-tech <afsharsogol@icloud.com>
2021-08-31 15:43:49 +0100

    Added source links.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,32 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Dictionary />
+        <br />
+        <footer className="footer">
+          <small>
+            This project was coded by{" "}
+            <a
+              href="https://github.com/sogol-tech"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Sogol Afshar
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/sogol-tech/dictionary-project.git"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              open-sourced on Github{" "}
+            </a>
+            and hosted on{" "}
+            <a href="https://nifty-leakey-734afc.netlify.app/" target="-blank">
+              Netlify
+            </a>
+          </small>
+        </footer>
       </div>
     </div>
   );
