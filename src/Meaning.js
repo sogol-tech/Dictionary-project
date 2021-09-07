d2ed37117a701c3e3733f725da83b297cee302ba
sogol-tech <afsharsogol@icloud.com>
2021-09-07 17:25:41 +0100

    Styling in CSS.

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,10 +5,12 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
+
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition: </strong>
               {definition.definition}
               <br />
               <em>{definition.example}</em>
